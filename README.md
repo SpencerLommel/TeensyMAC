8fe910bf81e2cfcb598497910221a02f38103d22
Spencer Lommel <spencerlommel@gmail.com>
2025-04-08 18:00:02 -0500

    v1.2.0 working

--- README.md
+++ README.md
@@ -10,6 +10,52 @@ This library supports the following boards:
 
 I don't have any of the other boards to test on so I cannot guarantee support for those. If someone would like to donate me one I'd be happy to add support here =D
 
+## Usage
+Here's a super simple C++ file that shows how to use this library
+```c++
+#include "TeensyMAC.h"
+
+// This is a simple function that prints out all of the bytes from the MAC address.
+// It esentially works the same way TeensyMAC::getMACString() does
+void printMACBytes(const uint8_t mac[6])
+{
+  Serial.print("MAC Bytes: ");
+  for (int i = 0; i < 6; i++)
+  {
+    if (i > 0)
+      Serial.print(":");
+    if (mac[i] < 16)
+      Serial.print("0");
+    Serial.print(mac[i], HEX);
+  }
+  Serial.println();
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+    delay(100); // Wait for serial connection
+  Serial.println("=== TeensyMAC Test ===");
+  // Get MAC string
+  String macStr = TeensyMAC::getMACString();
+  Serial.print("MAC String: ");
+  Serial.println(macStr);
+  // Get MAC bytes
+  uint8_t mac[6];
+  uint8_t *ptr = TeensyMAC::getMAC();
+  for (int i = 0; i < 6; i++)
+  {
+    mac[i] = ptr[i]; // copy each byte
+  }
+  // Print copied MAC bytes
+  printMACBytes(mac);
+}
+void loop()
+{
+  // Nothing in the loop
+}
+```
 
 # Notice a bug?
 Open a GitHub issue! I developed this library for a work project so I'd like to actively maintain it
--- TeensyMac.h
+++ TeensyMac.h
@@ -1,16 +1,52 @@
 // Spencer Lommel
-// Dec. 16th 2024
-// Header for TeensyMac4.cpp & TeensyMac3.cpp
+// TeensyMAC v1.2.0
+// Apr. 8th 2025
+// Updated Single header version
 
-#ifndef TEENSYMAC_H
-#define TEENSYMAC_H
+// No warranty, no license, use this code for whatever, enjoy =D
 
+#ifndef TEENSY_MAC_H
+#define TEENSY_MAC_H
 #include <Arduino.h>
-
+#if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__IMXRT1062__)
+#define TEENSY_HAS_MAC
+#endif
+#ifdef TEENSY_HAS_MAC
 class TeensyMAC {
 public:
-  static void getMAC(uint8_t mac[6]); // Get MAC address as an array
-  static String getMACString();       // Get MAC address as a string
+    static uint8_t* getMAC() {
+        static uint8_t mac[6];
+        #if defined(__MK20DX128__) || defined(__MK20DX256__)
+        // Teensy 3.0 and 3.1/3.2 use the unique ID registers
+        uint8_t *uid = (uint8_t *)0x40048058; // SIM_UIDMH
+        mac[0] = 0x02;
+        mac[1] = uid[0];
+        mac[2] = uid[1];
+        mac[3] = uid[2];
+        mac[4] = uid[3];
+        mac[5] = uid[4];
+        #elif defined(__IMXRT1062__)
+        // Generate MAC from UID (CFG0/CFG1)
+        uint32_t uid_high = HW_OCOTP_CFG0;
+        uint32_t uid_low  = HW_OCOTP_CFG1;
+        mac[0] = 0x02; // Locally administered MAC
+        mac[1] = (uid_high >> 24) & 0xFF;
+        mac[2] = (uid_high >> 16) & 0xFF;
+        mac[3] = (uid_low >> 24) & 0xFF;
+        mac[4] = (uid_low >> 16) & 0xFF;
+        mac[5] = (uid_low >> 8) & 0xFF;
+       #endif
+       return mac;
+   }
+   static String getMACString() {
+       uint8_t* mac = getMAC();
+       char buffer[18];
+       snprintf(buffer, sizeof(buffer), "%02X:%02X:%02X:%02X:%02X:%02X",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+       return String(buffer);
+   }
 };
-
-#endif // TEENSYMAC_H
+#else
+#error "This Teensy board is not supported by TeensyMAC."
+#endif // TEENSY_HAS_MAC
+#endif // TEENSY_MAC_H
